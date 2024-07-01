2eabebab9c71adb6988cf071d305b8d9b04e1b84
Anil <a.prasad@intershop.com>
2024-07-01 15:37:20 -0500

    Initial Setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+#!find . -type f -exec sed -i 's/devanil20/'$1'/g' {} +
+#!find . -type f -exec sed -i '' "s/devanil20/$1/g" {} +
+LC_CTYPE=C find . -type f -exec sed -i '' "s/devanil20/$1/g" {} +
+
